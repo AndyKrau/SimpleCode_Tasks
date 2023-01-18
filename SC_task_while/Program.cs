00558388ae0ad4a258df63c72e84d3e50a7a526d
rogue <rogue@ORENPC>
2023-01-18 13:21:25 +0700

    Добавьте файлы проекта.

---
+++ SC_task_while/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SC_task_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int i = 0;
+                Console.WriteLine("Введите количество повторений:");
+                int n = int.Parse(Console.ReadLine());
+                while (i < n)
+                {
+                    Console.WriteLine($"Hello in {i + 1} time!");
+                    i++;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally 
+            {
+                Console.WriteLine("Вывод закончен!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
