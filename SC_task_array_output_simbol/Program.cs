bddc9cbfbbd011bc3962e569ed74f4c2ac2ac11b
AndyKrau <andrey.krause@yandex.ru>
2023-01-24 23:18:07 +0700

    add: add task with methods

---
+++ SC_task_array_output_simbol/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//Methods
+//Вывод нужного количества символов в консоль
+
+namespace SC_task_methods_output_simbol
+{
+    internal class Program
+    {
+        static string OutputSimbol(char simbol, int quantity)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < quantity; i++)
+            {
+                result += simbol;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите символ который нужно повторить:");
+            char character = char.Parse(Console.ReadLine());
+            Console.Write("Введите количество повторений:");
+            int outputQuantity = int.Parse(Console.ReadLine());
+
+            string str = OutputSimbol(character, outputQuantity);
+            
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SC_task_methods_find_index/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+//Найти индекс первого вхождения элемента, аналогично методу IndexOf
+
+namespace SC_task_methods_find_index
+{
+    internal class Program
+    {
+        static int FindFirstIndex(int[] array, int index)
+        {
+            int firstIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == index)
+                {
+                    firstIndex = i;
+                    break;
+                }
+                else
+                    firstIndex = -1;
+            }
+            return firstIndex;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 56, -4, 34, -1, 56, 101 };
+
+            Console.WriteLine("{ 1, 2, 56, -4, 34, -1, 56, 101 }");
+            Console.Write("Введите число для поиска его индекса в массиве: ");
+
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Первое вхождение выбранного числа в массиве под индексом: " + FindFirstIndex(arr, number));
+            Console.ReadKey();
+        }
+    }
+}
