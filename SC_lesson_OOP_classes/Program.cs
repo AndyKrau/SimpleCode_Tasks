5fa4322956d57d1a34e04578cb44a0d048a1f1a7
AndyKrau <andrey.krause@yandex.ru>
2023-02-03 15:39:49 +0700

    add: OOP lesson, student card example

--- SC_lesson_OOP_classes/Program.cs
+++ SC_lesson_OOP_classes/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System.Reflection.PortableExecutable;
+using System.Security.AccessControl;
+//Пример ООП на примере создания карточек студента,
+//реализованы public методы вывода информации в консоль, 
+// метод изменения поля фамилии,
+// метод вывода полной ФИО в констоль.
+// Также инкапсулированны локальные переменные класса и
+// созданы 3 перегрузки конструктора для различных методов добавления карточек.
+// Данный пример реализован с целью ознакомления некоторых тем.
 
 namespace SC_lesson_OOP_classes
 {
-    
     internal class Program
     {
         static void Main(string[] args)
         {
             Student firstStudent = new Student("Иванов", "Иван", "Михайлович", 23, "ЙЦУКЕН-65");
-
             firstStudent.Print();
+            firstStudent.ChangeLastname("########");
+            Console.WriteLine($"Полное имя студента:{firstStudent.GetFullName()}");
 
-            string fullName = firstStudent.GetFullName();
+            Console.WriteLine();
 
-            Console.WriteLine($"Полное имя студента:{fullName}");
+            Student secondStudent = new Student(firstStudent);
+            secondStudent.ChangeLastname("Петросян");
+            secondStudent.Print();
+            Console.WriteLine($"Полное имя студента:{secondStudent.GetFullName()}");
+
             Console.ReadLine();
         }
     }
--- SC_lesson_OOP_classes/Student.cs
+++ SC_lesson_OOP_classes/Student.cs
@@ -25,6 +25,26 @@ namespace SC_lesson_OOP_classes
             _middlename = middlename;
             _age = age;
             _group = group;
+            Console.WriteLine($"Создана карточка студента: {_lastname} {_firstname[0]}.{_middlename[0]}");
+        }
+
+        public Student(string lastname, string group)
+        {
+            _id = Guid.NewGuid();
+            _lastname= lastname;
+            _group = group;
+            Console.WriteLine($"Создана карточка студента: {_lastname}");
+        }
+
+        public Student(Student student)
+        {
+            _id = Guid.NewGuid();
+            _firstname = student._firstname;
+            _lastname = student._lastname;
+            _middlename = student._middlename;
+            _age = student._age;
+            _group = student._group;
+            Console.WriteLine($"Выполенено копирование карточки стдента {_lastname} {_firstname[0]}.{_middlename[0]}");
         }
 
         private Guid _id;
@@ -46,6 +66,16 @@ namespace SC_lesson_OOP_classes
             Console.WriteLine($"Отчество: {_middlename}");
             Console.WriteLine($"Возраст: {_age}");
             Console.WriteLine($"Группа: {_group}");
+        }
+        /// <summary>
+        /// Меняет фамилию студента
+        /// </summary>
+        /// <param name="newLastname">Новая фамилия студента</param>
+        public void ChangeLastname(string newLastname)
+        {
+            _lastname = newLastname;
+
+            Console.WriteLine($"Фамилия была изменена на: {newLastname}");
         }
 
         /// <summary>
